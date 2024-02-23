7a8bd89272207110a91592bfbceea4fe51023b93
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2024-02-23 14:18:56 -0800

    Can play music from queried stations.

--- radio-test/src/components/Radio.jsx
+++ radio-test/src/components/Radio.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { RadioBrowserApi } from "radio-browser-api";
+import AudioPlayer from "react-h5-audio-player";
+import defaultImg from "../assets/doge.png";
 
 export default function Radio() {
   const [stations, setStations] = useState();
   const [stationFilter, setStationFilter] = useState("all");
 
+  useEffect(() => {
+    setup(stationFilter).then((data) => {
+      setStations(data);
+    });
+  }, [stationFilter]);
+
   const setup = async (stationFilter) => {
-    const api = new RadioBrowserApi("My Radio App");
+    const api = new RadioBrowserApi(fetch.bind(window), "My Radio App");
+
+    const stations = await api.searchStations({ language: "english", tag: stationFilter, limit: 30 }).then((data) => {
+      return data;
+    });
+
+    return stations;
   };
 
   const filters = ["all", "rap", "rock", "country", "blues", "jazz"];
+
+  const setDefaultSrc = (event) => {
+    event.target.src = defaultImg;
+  };
 
   return (
     <div className="radio">
@@ -24,6 +42,33 @@ export default function Radio() {
             </h3>
           );
         })}
+      </div>
+      <div className="stations">
+        {stations &&
+          stations.map((station, index) => {
+            return (
+              <>
+                <div
+                  className="station"
+                  key={index}>
+                  <div className="stationName">
+                    <img
+                      className="logo"
+                      src={station.favicon}
+                      alt="station logo"
+                      onError={setDefaultSrc}
+                    />
+                  </div>
+                  <div className="name">{station.name}</div>
+                </div>
+
+                <AudioPlayer
+                  className="player"
+                  src={station.urlResolved}
+                />
+              </>
+            );
+          })}
       </div>
     </div>
   );
